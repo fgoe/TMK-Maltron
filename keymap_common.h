92a33215f331c3a28e57a0afb3ed306956366092
Florian Götz <f.goetz@mailbox.org>
2016-04-26 18:31:56 +0200

    Added files via upload

--- keymap_common.h
+++ keymap_common.h
@@ -43,16 +43,16 @@ extern const uint16_t fn_actions[];
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, \
     K40, K41, K42, K43, K44, K45, K46, K47, K48,      K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, \
-         K51, K52, K53, K54, K55, K56, K57,                K5B, K5C, K5D, K5E, K5F, K5G, K5H,  \
-	                               K67,                K6B \
+         K51, K52, K53, K54,      K56, K57, K58,      K5A, K5B, K5C,      K5E, K5F, K5G, K5H,  \
+	                                    K68,      K6A \
 ) { \
     { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E, KC_##K0F, KC_##K0G, KC_##K0H, KC_##K0I }, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I }, \
     { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D, KC_##K2E, KC_##K2F, KC_##K2G, KC_##K2H, KC_##K2I }, \
     { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D, KC_##K3E, KC_##K3F, KC_##K3G, KC_##K3H, KC_##K3I }, \
     { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_NO   , KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K4E, KC_##K4F, KC_##K4G, KC_##K4H, KC_##K4I }, \
-    { KC_NO   , KC_##K51, KC_##K52, KC_##K53, KC_##K54, KC_##K55, KC_##K56, KC_##K57, KC_NO   , KC_NO   , KC_NO   , KC_##K5B, KC_##K5C, KC_##K5D, KC_##K5E, KC_##K5F, KC_##K5G, KC_##K5H, KC_NO    },  \
-    { KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_##K67, KC_NO   , KC_NO   , KC_NO   , KC_##K6B, KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO    }  \
+    { KC_NO   , KC_##K51, KC_##K52, KC_##K53, KC_##K54, KC_NO   , KC_##K56, KC_##K57, KC_##K58, KC_NO   , KC_##K5A, KC_##K5B, KC_##K5C, KC_NO   , KC_##K5E, KC_##K5F, KC_##K5G, KC_##K5H, KC_NO    },  \
+    { KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_##K68, KC_NO   , KC_##K6A, KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO    }  \
 }
 
 
